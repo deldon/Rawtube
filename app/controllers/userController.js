a58aaabddfecada991583f630c1bd71461c264c1
MarkSoclock <mark.smaniotto@gmail.com>
2022-06-07 15:28:38 +0200

    Created userController with addUser method

---
+++ app/controllers/userController.js
@@ -0,0 +1,34 @@
+const debug = require('debug')('userController');
+const bcrypt = require('bcrypt');
+// const jwt    = require('jsonwebtoken');
+// const SECRET_KEY = process.env.SECRET_KEY;
+// const url_avatar = process.env.URL_SERVER + 'avatar/';
+
+module.exports = {
+
+    addUser: async (req, res, next) => {
+
+        if (req.body.new_password == req.body.confirm_new_password) {
+            const salt = await bcrypt.genSalt(10);
+            const encryptedPassword = await bcrypt.hash(req.body.new_password, salt);
+    
+                const form = {
+                    name: req.body.name,
+                    email: req.body.email,
+                    password: encryptedPassword,
+                    description: req.body.description,
+                    avatar: 'thumbnail.png',
+                }
+     
+            const newUser = await DataMapper.addUser(form);
+            if (newUser) {
+                debug(`> addUser()`);
+                delete newUser.password;
+                res.json(newUser);
+            } else {
+                next();
+            }
+        }
+
+    }
+}
--- app/routers/router.js
+++ app/routers/router.js
@@ -4,8 +4,10 @@ const router = express.Router();
 const controllerHandler = require('../helpers/controllerHandler');
 
 const mainController = require('../controllers/mainController');
+const userController = require('../controllers/userController');
 
 router.get('/', controllerHandler(mainController.helloWord));
+router.post('/user', controllerHandler(userController.addUser));
 
 
 module.exports = router;

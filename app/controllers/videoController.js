ca24a1ccc3190664db59face1f90ac05edd83052
deldon <r.deldon@gmail.com>
2022-06-09 15:57:22 +0200

    adding getVideoById and getVideoByRealaseDate

--- app/controllers/videoController.js
+++ app/controllers/videoController.js
@@ -1,12 +1,40 @@
 const debug = require('debug')('videoController');
+const videoDataMapper = require('../dataMapper/VideoDataMapper')
 
 module.exports = {
-    getVideoById: (req,res)=>{ 
-        res.render('pages/watch',{id:req.query.v})
+    getVideoById: async (req, res) => {
+        const data = await videoDataMapper.getVideoById(req.query.v);
+        debug('getVideoById called');
+        if (data) {
+
+            res.render('pages/watch', { data })
+        } else {
+            next();
+        }
+
     },
 
-    getVideoByRelevance:  (req,res)=>{ 
-        res.render('pages/index')
+    getVideoByRelevance: async (req, res) => {
+
+
+        const data = await videoDataMapper.getVideoByReleaseDate();
+        debug('getVideoByRelevance called');
+        if (data) {
+
+            data.map((x)=>{
+
+                const date = new Date(0);
+                date.setSeconds(x.duration); // specify value for SECONDS here
+                x.duration = date.toISOString().substr(11, 8);
+            })
+
+
+            res.render('pages/index', { data })
+        } else {
+            next();
+        }
+
+
     },
 
 }
---
+++ app/dataMapper/VideoDataMapper.js
@@ -0,0 +1,58 @@
+const debug = require('debug')('videoDataMapper');
+const dataBase = require('../config/db');
+const ApiError = require('../errors/apiError');
+
+module.exports = {
+  async getVideoById(id) {
+
+    const query = `SELECT 
+    rawtube_video.id,
+    rawtube_video.title,
+    rawtube_video.url_file,
+    rawtube_video.duration,
+    rawtube_video.description,
+    rawtube_video.views,
+    rawtube_user.id AS user_id,
+    rawtube_user.name AS user_name,
+    rawtube_user.avatar AS user_avatar
+    FROM rawtube_video
+    JOIN rawtube_user ON rawtube_user.id = rawtube_video.user_id
+    WHERE rawtube_video.public = true
+    AND rawtube_video.id = $1;`;
+
+    const values = [id]
+
+    const data = (await dataBase.query(query,values)).rows[0];
+    debug(`> getVideoById()`);
+    if (!data) {
+      throw new ApiError('No data found for > getVideoById()', 400);
+    }
+    
+    return data;
+  },
+    async getVideoByReleaseDate() {
+
+        const query = `SELECT 
+        rawtube_video.id,
+        rawtube_video.title,
+        rawtube_video.url_thumbnail,
+        rawtube_video.duration,
+        rawtube_video.views,
+        rawtube_user.id AS user_id,
+        rawtube_user.name AS user_name,
+        rawtube_user.avatar AS user_avatar
+        FROM rawtube_video
+        JOIN rawtube_user ON rawtube_user.id = rawtube_video.user_id
+        WHERE rawtube_video.public = true
+        ORDER BY rawtube_video.release_date;`;
+
+        const data = (await dataBase.query(query)).rows;
+        debug(`> getVideoByReleaseDate()`);
+        if (!data) {
+          throw new ApiError('No data found for > addUser()', 400);
+        }
+        
+        return data;
+      },
+
+}

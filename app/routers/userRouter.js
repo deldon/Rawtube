9e2f3575580da14307c19c89cfea5d6bcafb3364
MarkSoclock <mark.smaniotto@gmail.com>
2022-10-05 18:43:12 +0200

    Finished APIdoc for user

--- app/routers/userRouter.js
+++ app/routers/userRouter.js
@@ -127,6 +127,24 @@ router.post('/user/register', controllerHandler(userController.addUser)); //new
  */
 router.patch('/user/',security.check, controllerHandler(userController.updateUser));
 
+/**
+ * User
+ * @typedef {object} UserThumbnail
+ * @property {string} sampleFile - Thumbnail file - binary
+ */
+/**
+ * PATCH /user/thumbnail/
+ * @summary upload a thumbnail
+ * @tags USER
+ * @param {UserThumbnail} request.body.required - Thumbnail file - multipart/form-data
+ * @return {object} 200 - success response
+ * @security BearerAuth
+ * @example response - 200 - example success response
+ * {
+ *  "url_thumbnail": "ba058e37-4f8e-4ce9-9a51-6e751d121260.jpg",
+ *  "uploaded": true
+ * }
+ */
 router.patch('/user/thumbnail/',security.check, controllerHandler(uploadController.uploadUserThumbnail));
 
 router.delete('/user/:user_id', controllerHandler(userController.deleteUser));
